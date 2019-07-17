e311e31590ee7abe206fb686e9d3cf691a6acc73
msj521 <1053804748@qq.com>
2019-07-17 15:23:28 +0800

    修改数据库配置

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.100.226.80',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'webim',
+    'database'        => 'table',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'msj586',
+    'password'        => '123456',
     // 端口
     'hostport'        => '',
     // 连接dsn
